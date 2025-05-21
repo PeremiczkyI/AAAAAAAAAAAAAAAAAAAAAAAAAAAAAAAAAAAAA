266f9c51c5f5bb72ae4ca1a187968c29ddcf0e10
KovacsSzabolcs <90681643+Kszaboli@users.noreply.github.com>
2025-05-21 19:45:38 +0200

    Add files via upload

---
+++ Consol/Helsinki_Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helsinki1952
+{
+    public class Versenyzo
+    {
+        public Versenyzo(string sor)
+        {
+            string[] temp = sor.Split(' ');
+
+            Helyezes = int.Parse(temp[0]);
+            VersenyzokSzama = int.Parse(temp[1]);
+            Sportag = temp[2];
+            Versenyszam = temp[3];
+        }
+
+        public int Helyezes { get; private set; }
+        public int VersenyzokSzama { get; private set; }
+        public string Sportag { get; private set; }
+        public string Versenyszam { get; private set; }
+    }
+}
